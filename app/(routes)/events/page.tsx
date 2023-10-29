8378d899cae0a71afe5faf9c30ccc98d8543cd94
lmatuzic <luka.matuzic1@gmail.com>
2023-10-29 15:32:46 +0100

    changed pages to routes, updated next js version to 14

--- app/(routes)/events/page.tsx
+++ app/(routes)/events/page.tsx
@@ -2,7 +2,7 @@ export default async function EventsPage() {
 	return (
 		<>
 			<header className='flex items-center justify-between pb-4'>
-				<h1 className='text-2xl'>Events</h1>
+				<h1>Events</h1>
 			</header>
 		</>
 	);
