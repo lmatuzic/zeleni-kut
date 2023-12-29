55eaeaec21ba611dc375ff5044af5df99801d7dd
lmatuzic <luka.matuzic1@gmail.com>
2023-12-29 12:47:49 +0100

    commit

--- next.config.js
+++ next.config.js
@@ -5,13 +5,12 @@ module.exports = {
 		HYGRAPH_CONTENT_API:
 			'https://api-eu-central-1-shared-euc1-02.hygraph.com/v2/clc6b0m3n18b201uldropgzhq/master',
 		RESEND_API_KEY: 're_StLNfNeG_GLRGZ2mcomfrNWmcsrVtv2SB',
-		HYGRAPH_IMAGES_HOSTNAME: 'media.graphassets.com',
 	},
 	images: {
 		remotePatterns: [
 			{
 				protocol: 'https',
-				hostname: this.env.HYGRAPH_IMAGES_HOSTNAME,
+				hostname: 'media.graphassets.com',
 				port: '',
 				pathname: '/**',
 			},
