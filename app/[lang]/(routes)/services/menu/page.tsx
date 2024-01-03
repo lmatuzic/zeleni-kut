98720c9ac6d72a0504acb26c6418d7a3b85b12d9
lmatuzic <luka.matuzic1@gmail.com>
2024-01-03 18:48:21 +0100

    add missing Locale type

--- app/[lang]/(routes)/services/menu/page.tsx
+++ app/[lang]/(routes)/services/menu/page.tsx
@@ -1,4 +1,5 @@
 import MenuList from './components/MenuList';
+import { Locale } from '@/i18.config';
 
 export default function Menu({ params: { lang } }: { params: { lang: Locale } }) {
 	return (
