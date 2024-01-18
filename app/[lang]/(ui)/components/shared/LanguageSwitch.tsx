a71b6f0868f24dcef927f73cbcbd12a8721ec48f
lmatuzic <luka.matuzic1@gmail.com>
2024-01-18 09:56:46 +0100

    clean up locale icon render

--- app/[lang]/(ui)/components/shared/LanguageSwitch.tsx
+++ app/[lang]/(ui)/components/shared/LanguageSwitch.tsx
@@ -1,17 +1,11 @@
 'use client';
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { usePathname } from 'next/navigation';
-import { Locale, i18n } from '@/i18.config';
 import CroatiaIcon from '@/app/[lang]/assets/icons/croatia-icon.svg';
 import UKIcon from '@/app/[lang]/assets/icons/uk-icon.svg';
-
-// Map locales to their respective icons
-const localeIconMap = {
-	hr: CroatiaIcon,
-	en: UKIcon,
-};
+import { i18n } from '@/i18.config';
+import Image from 'next/image';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 export default function LanguageSwitch() {
 	const pathName = usePathname();
@@ -23,46 +17,23 @@ export default function LanguageSwitch() {
 
 		const segments = pathName.split('/');
 		segments[1] = locale;
-
 		return segments.join('/');
-	};
-
-	const renderCountryFlag = (locale: Locale) => {
-		if (locale === 'hr') {
-			return (
-				<Image
-					src={CroatiaIcon}
-					alt='croatia-icon'
-					width={30}
-					height={30}
-					className='rounded-full mr-2'
-					priority
-				/>
-			);
-		}
-
-		return (
-			<Image
-				src={UKIcon}
-				alt='croatia-icon'
-				width={30}
-				height={30}
-				className='rounded-full'
-				priority
-			/>
-		);
 	};
 
 	return (
 		<ul className='flex'>
 			{i18n.locales.map((locale) => {
-				const IconComponent = localeIconMap[locale]; // Get the corresponding icon component
-
 				return (
 					<li key={locale}>
 						<Link href={redirectedPathName(locale)}>
-							{renderCountryFlag(locale)}
-							{/* {locale} */}
+							<Image
+								src={locale === 'hr' ? CroatiaIcon : UKIcon}
+								alt={locale === 'hr' ? 'croatian language' : 'english language'}
+								width={30}
+								height={30}
+								className='rounded-full mr-2'
+								priority
+							/>
 						</Link>
 					</li>
 				);
