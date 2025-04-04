8108dc0ee4e0e51470caee79b52923d02b4249e0
Luka Matuzic <luka.matuzic@leapwise.co>
2025-04-04 12:11:49 +0200

    chore: next font import path update

--- app/[lang]/layout.tsx
+++ app/[lang]/layout.tsx
@@ -1,5 +1,5 @@
 import { Locale, i18n } from '@/i18.config';
-import { Comfortaa } from '@next/font/google';
+import { Comfortaa } from 'next/font/google';
 import { Analytics } from '@vercel/analytics/react';
 import { SpeedInsights } from '@vercel/speed-insights/next';
 import type { Metadata } from 'next';
